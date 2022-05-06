437c76241e85fe3a05da98425e56a7a92d9b51ae
hyein8290 <hyein8290@gmail.com>
2022-05-06 17:57:35 +0900

    commit

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -9,7 +10,6 @@ namespace ClientTest
 {
     static class Program
     {
-
         static void Main(string[] args)
         {
             M1();
@@ -22,10 +22,54 @@ namespace ClientTest
             TcpClient client = new TcpClient();
             client.Connect("127.0.0.1", 9000);
 
-            byte[] buf = Encoding.UTF8.GetBytes("클라이언트 : 접속합니다");
-            client.GetStream().Write(buf, 0, buf.Length);
+            NetworkStream ns = client.GetStream();
+            //StreamReader sr = new StreamReader(ns);
+            //StreamWriter sw = new StreamWriter(ns);
 
-            client.Close();
+            byte[] buf = Encoding.UTF8.GetBytes("클라이언트 : 접속합니다");
+            ns.Write(buf, 0, buf.Length);
+
+            //ns.Close();
+            //client.Close();
+
+            //sw.WriteLine("client");
+            //sw.Flush();
+            //ns.Flush();
+
+            //client.Close();
+        }
+
+        private static void M2()
+        {
+            Console.WriteLine("클라이언트콘솔창.");
+
+            TcpClient client = new TcpClient();
+            
+            try
+            {
+                client.Connect("127.0.0.1", 9000);
+            }
+            catch
+            {
+                Console.WriteLine("서버 연결에 실패했습니다.");
+            }
+            
+            Console.WriteLine("서버 연결에 성공했습니다.");
+
+            while (true)
+            {
+                Console.Write("보낼 메시지: ");
+
+                string message = Console.ReadLine();
+
+                byte[] byteData = new byte[message.Length];
+                byteData = Encoding.Default.GetBytes(message);
+
+                client.GetStream().Write(byteData, 0, byteData.Length);
+            }
+
+
+
         }
 
 
